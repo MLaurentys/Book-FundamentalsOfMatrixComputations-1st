8139074aafc75d1801598b126be53090cd36af5a
MLaurentys <matheus_t.laurentys@hotmail.com>
2018-08-12 19:39:18 -0400

    matrix multiplication. Adding flop count to inner product

--- Lib/MyMath.cpp
+++ Lib/MyMath.cpp
@@ -13,13 +13,15 @@ namespace MyMath {
         }
         return column;
     }
-    float dotProduct(std::vector<float> vector1, std::vector<float> vector2) {
+  std::tuple<float,float> dotProduct(std::vector<float> vector1, std::vector<float> vector2) {
         //assume vector1.size() = vector2.size()
         float sum = 0;
+	float flopCount = 0;
         for (int i = 0; i < vector1.size(); ++i) {
             sum += vector1[i] * vector2[i];
+	    flopCount += 2; //1 multiplication + 1 sum
         }
-        return sum;
+        return std::make_tuple(sum,flopCount);
     }
 
     float power(float number, float exponent) {
@@ -45,5 +47,19 @@ namespace MyMath {
             }
         }
         return retMatrix;
+    }
+	std::tuple<int, std::vector<std::vector<float>> multiplyMatrix(std::vector<std::vector<float>> matrix1, std::vector<std::vector<float>> matrix2){
+	  std::vector<std::vector<float>> retMatrix;
+	  int flopCount = 0;
+	  float cellAux;
+	  float flopAux;
+	  for(int i = 0; i < matrix1.size(); ++i){
+	    retMatrix.push_back(std::vector<float>);
+	    for(int j = 0; j < matrix1[0].size(); ++j){
+	      std::tie(cellAux, flopAux) =  MyMath::dotProduct(matrix1[i], MyMath::getColumn(matrix2, j));
+	      retMatrix[i][j].push_back(cellAux);
+	      flopCount += flopAux;
+	    }
+      }
     }
 }
--- Lib/MyMath.h
+++ Lib/MyMath.h
@@ -2,13 +2,15 @@
 #ifndef MYMATH_H_
 #define MYMATH_H_
 #include <vector>
-
+#include <tuple>
 namespace MyMath {
     extern void addIndentity(std::vector<std::vector<float>>& matrix, float scalar = 1);
     extern std::vector<float> getColumn(const std::vector<std::vector<float>>& matrix, int index);
-    extern float dotProduct(std::vector<float> vector1, std::vector<float> vector2);
+  extern std::tuple<float, float dotProduct(std::vector<float> vector1, std::vector<float> vector2);
     extern float power(float num, float exponent);
     extern void scalarTimesVector(std::vector<float>& vector, float scalar);
     extern std::vector<std::vector<float>> outerProduct(std::vector<float>& vec1, std::vector<float>& vec2);
+    extern std::tuple<int, std::vector<std::vector<float>> multiplyMatrix(
+	    std::vector<std::vector<float>> matrix1, std::vector<std::vector<float>> matrix2);
 }
 #endif
